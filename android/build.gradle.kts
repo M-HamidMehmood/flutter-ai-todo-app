3ec99e014e2c07fa1cfcf6b31356eb1d2c39fa2d
Hamid Mehmood <saim008865@gmail.com>
2025-05-07 07:19:55 +0500

    chore: refactor Kotlin version declaration in build.gradle.kts for improved readability

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,14 +1,13 @@
 buildscript {
-    ext {
-        kotlin_version = "1.8.22"
-    }
+    val kotlinVersion = "1.8.22"
+    
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
 
